3b5ec4a98b452db18214215a14b298aa44ace5b6
solomon.sitotaw23 <solomon.sitotaw@hahu.jobs>
2022-11-29 13:55:46 +0300

    photo changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import myPhoto from "../assets/my_photo1.png";
+import myPhoto from "../assets/my-photo.jpg";
 import FileSaver from "file-saver";
 
 const Home = () => {
